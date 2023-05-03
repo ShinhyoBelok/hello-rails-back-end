35c5da785cfd133fc1283fcd31998098c4ee531a
ShinhyoBelok <shibo6937@gmail.com>
2023-05-03 18:24:59 -0400

    Add api greetings controller

---
+++ test/controllers/api_greetings_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class ApiGreetingsControllerTest < ActionDispatch::IntegrationTest
+  test "should get random" do
+    get api_greetings_random_url
+    assert_response :success
+  end
+end
